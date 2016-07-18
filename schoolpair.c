62a759f330b9ea0b9fb93983448ebff0fb79e9cf
sivapraveen20 <sivatsp@ymail.com>
2016-07-18 10:05:11 +0530

    Create schoolpair.c

---
+++ schoolpair.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,x,b,i,j=1;
+clrscr();
+printf("Enter the value of n : ");
+scanf("\n %d",&a);
+printf("Enter the pair value : ");
+scanf("\n %d",&b);
+for(k=0;k<2*n;k++)
+{
+x=2*a-k;
+if(b!=x)
+{
+printf("The pair is%d for days %d is %d\n",b,j++,b);
+}
+}
+getch();
+}
